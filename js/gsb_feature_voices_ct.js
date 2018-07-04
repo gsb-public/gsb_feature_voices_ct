ff3de4b30ccebd5d077f4dcf2de21528ac3fc4a5
Roopesh Naik <rnaik77@stanford.edu>
2018-07-04 15:08:27 +0530

    WP-8386 fix for initial condition for location and details page

--- js/gsb_feature_voices_ct.js
+++ js/gsb_feature_voices_ct.js
@@ -1,14 +1,18 @@
 (function ($) {
   Drupal.behaviors.gsb_feature_voices_ct = {
     attach: function (context, settings) {
+      var person_type = ["42756", "42761", "42766", "42771"]; // MBA Alumni, MSx and Sloan Alumni, Exec Ed, Seed
+      var location_label = $("label[for*='edit-field-location']");
+      if ($.inArray($('input[name=field_person_type[und]]:checked' ).val(), person_type) != -1) {
+        location_label.html("Location");
+      }
 
-     $('input:radio[name=field_person_type[und]]').change(function() {
-        var person_type = ["42756", "42761", "42766", "42771"]; // MBA Alumni, MSx and Sloan Alumni, Exec Ed, Seed
+      $('input:radio[name=field_person_type[und]]').change(function() {
         if ($.inArray(this.value, person_type) != -1) {
-          $("label[for*='edit-field-location']").html("Location");
+          location_label.html("Location");
         }
         else {
-          $("label[for*='edit-field-location']").html("Hometown");
+          location_label.html("Hometown");
         }
       });
     }
